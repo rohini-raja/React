09f59626c1d29624454df6b9c860acc0e0190f1d
Rohini R <rohinimangai14@gmail.com>
2024-11-03 21:34:47 +0530

    Using Swiggy api to display the data
    its so cool to use swiggy's api to fetch data damn

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,10 @@
-import RestaurantCard from "./RestaurantCard";
-import {resList} from "../utils/mockdata";
 import { useEffect, useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+//import {resList} from "../utils/mockdata";
+
 const Body = () => {
-    console.log(resList);
-    const [listOfRestaurants,setListofRestaurants] = useState(resList);
+   // console.log(resList);
+    const [listOfRestaurants,setListofRestaurants] = useState([]);
     console.log("listofres:", listOfRestaurants);
 
     useEffect(
@@ -12,11 +13,12 @@ const Body = () => {
             fetchData();
         },[]);
         const fetchData = async () => {
-           // const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9709162&lng=80.2467688&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-           const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0832022&lng=80.27552519999999&collection=138501&tags=&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
+           const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9709162&lng=80.2467688&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+          // https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
+           //const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0832022&lng=80.27552519999999&collection=138501&tags=&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
             const json = await data.json(); //convert the data to json
             console.log("json :", json);
-            setListofRestaurants(json.data.cards[2].card.card.info);
+            setListofRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         };
 
         console.log("Body rendeered");
